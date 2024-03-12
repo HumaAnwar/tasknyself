2552bc6a1588e707a807a69c5b0a89b0d56d553f
Madiha Khalid123 <madihakhalid403@gmail.com>
2024-03-12 10:44:59 +0500

    work in backend

--- app/src/main/java/com/HISkyTech/LoginScreen/Ui/different_tasks.kt
+++ app/src/main/java/com/HISkyTech/LoginScreen/Ui/different_tasks.kt
@@ -1,12 +1,40 @@
 package com.HISkyTech.LoginScreen.Ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.HISkyTech.LoginScreen.R
+import com.HISkyTech.LoginScreen.databinding.ActivityDifferentTasksBinding
 
 class different_tasks : AppCompatActivity() {
+    private lateinit var binding: ActivityDifferentTasksBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+      binding= ActivityDifferentTasksBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_different_tasks)
+        setContentView(binding.root)
+        binding.work.setOnClickListener(){
+
+        }
+        binding.home.setOnClickListener(){
+
+        }
+        binding.Music.setOnClickListener(){
+
+        }
+        binding.food.setOnClickListener(){
+
+        }
+        binding.bills.setOnClickListener(){
+
+        }
+        binding.study.setOnClickListener(){
+
+        }
+        binding.travel.setOnClickListener(){
+
+        }
+        binding.shopping.setOnClickListener(){
+            
+        }
     }
 }
