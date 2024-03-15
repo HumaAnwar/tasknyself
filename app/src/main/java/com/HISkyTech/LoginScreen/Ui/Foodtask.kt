a60cc834c92dd36e0166b28462920939f31d400c
Huma Anwar <huma01122@gmail.com>
2024-03-15 12:16:56 +0500

    food

---
+++ app/src/main/java/com/HISkyTech/LoginScreen/Ui/Foodtask.kt
@@ -0,0 +1,12 @@
+package com.HISkyTech.LoginScreen.Ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.HISkyTech.LoginScreen.R
+
+class Foodtask : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_foodtask)
+    }
+}
