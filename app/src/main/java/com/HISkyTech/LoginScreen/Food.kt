9994894b6bce3e6e301b351d288411519472e690
Huma Anwar <huma01122@gmail.com>
2024-03-15 11:52:45 +0500

    LAYOUTS

--- app/src/main/java/com/HISkyTech/LoginScreen/Food.kt
+++ app/src/main/java/com/HISkyTech/LoginScreen/Food.kt
@@ -2,18 +2,9 @@ package com.HISkyTech.LoginScreen
 
 import android.app.AlertDialog
 import android.app.Dialog
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.Toast
-import com.HISkyTech.LoginScreen.Models.food_task
-import com.HISkyTech.LoginScreen.Models.task_model
 import com.HISkyTech.LoginScreen.databinding.ActivityFoodBinding
-import com.HISkyTech.LoginScreen.databinding.ActivityHomeBinding
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
@@ -33,7 +24,7 @@ class Food : AppCompatActivity() {
             builder.setTitle("Select Option")
 
                 .setPositiveButton("Add new Task") { dialog, which ->
-                 
+
 
                 }
                 .setNegativeButton("Cancel") { dialog, which ->
